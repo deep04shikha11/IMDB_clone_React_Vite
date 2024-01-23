73bbbf43139cf90f5e40b6f8902678f048760726
deepshikha <deepshikhaverma44@gmail.com>
2024-01-23 15:35:55 +0530

    Add routing between components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
-import Banner from "./components/Banner"
-import Navbar from "./components/Navbar"
+import Banner from "./components/Banner";
+import Movies from "./components/Movies";
+import Navbar from "./components/Navbar";
+import WatchList from "./components/WatchList";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <>
-    <Navbar/>
-    <Banner/>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<><Banner /> <Movies /></>} />
+          <Route path="/watchlist" element={<WatchList />} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
